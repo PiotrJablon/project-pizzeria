2f939a9944deb384714675d1cf31e2aff89eb7b4
Piotrek <pjablonski123.00@wp.pl>
2021-02-03 16:54:32 +0100

    add imageWrapper to get elements

--- src/js/script.js
+++ src/js/script.js
@@ -71,6 +71,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
 
     }
     initAccordion(){
@@ -126,6 +127,7 @@
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
+          console.log(option.price)
 
           if(formData[paramId] && formData[paramId].includes(optionId)) {
             if(option != 'default') {
